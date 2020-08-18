4f5eb14e0fe927e8614fce2c48da8ad04bf2a009
Amber Taranto <ambzzz@gmail.com>
2020-08-17 21:19:49 -0700

    added more seed data to test

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,3 @@
-location = Location.create(name: 'Location', description: 'home', favorite: 'true', destination: 'false', visited: 'true')
+Location.create(name: 'Location', description: 'home', favorite: 'true', destination: 'false', visited: 'true')
+Location.create(name: 'Carlsbad', description: 'SD', favorite: 'false', destination: 'false', visited: 'false')
+Location.create(name: 'Deming', description: 'parents', favorite: 'true', destination: 'true', visited: 'true')
